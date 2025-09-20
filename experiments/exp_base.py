e01100fa55e4b272261c2f6655ff24986afc323d
Fabio <fabio.lanzi@unimore.it>
2025-09-20 21:21:53 +0200

    refactor BaseExperiment and ToySortExperiment with detailed docstrings and type hints

--- experiments/exp_base.py
+++ experiments/exp_base.py
@@ -1,13 +1,28 @@
-import torch
-from torch import nn, optim, device
-from torch.utils.data import DataLoader
-from utils.logger import Logger
-import torch.nn.functional as F
-from argparse import ArgumentParser
-from configs.config import Config
-import os
+"""
+Module containing the BaseExperiment class for managing AI/ML experiments.
+"""
 
-class BaseExperiment():
+from abc import ABC, abstractmethod
+
+import torch
+from torch import nn, optim
+from torch.utils.data import DataLoader
+
+from configs.config import Config
+from utils.logger import Logger
+
+
+class BaseExperiment(ABC):
+    """
+    Abstract base class that defines the structure of a basic ML/AI experiment.
+
+    This class provides a template for training, validation, and inference
+    procedures while also handling logging and model checkpointing.
+
+    Subclasses are expected to implement the `train`, `validate`, and
+    `inference` methods according to their specific use case.
+    """
+
     def __init__(
         self,
         model: nn.Module,
@@ -15,17 +30,19 @@ class BaseExperiment():
         optimizer: optim.Optimizer,
         device: torch.device,
         logger: Logger,
-        conf: Config
+        conf: Config,
     ):
         """
-        Initializes the BaseExperiment class with the provided model, criterion, optimizer, device, logger, and arguments.
+        Initializes the BaseExperiment class with the provided model, criterion,
+        optimizer, device, logger, and arguments.
+
         Args:
-            model (nn.Module): The model to be used in the experiment.
-            criterion (nn.Module): The loss function to be used.
-            optimizer (optim.Optimizer): The optimizer to be used for training.
-            device (torch.device): The device (CPU or GPU) on which the model will be trained.
-            logger (Logger): Logger instance for logging experiment results.
-            conf (Config): Configuration class
+            model: The model to be used in the experiment.
+            criterion: The loss function to be used.
+            optimizer: The optimizer to be used for training.
+            device: The device (CPU or GPU) on which the model will be trained.
+            logger: Logger instance for logging experiment results.
+            conf: Configuration class
         """
         self.model = model
         self.criterion = criterion
@@ -33,82 +50,104 @@ class BaseExperiment():
         self.device = device
         self.logger = logger
         self.conf = conf
-        self.min_metric_model = float("inf") # The minimum metric value for model saving
-        
+        self.min_metric_model = float("inf")  # minimum metric value for model saving
+
+    @abstractmethod
     def train(self, train_loader: DataLoader) -> float:
         """
         Train the model for one epoch using the provided DataLoader.
+
         Args:
-            train_loader (DataLoader): DataLoader for the training dataset.
-        
+            train_loader: DataLoader for the training set.
+
         Returns:
-            float: The average training loss for the epoch.
+            Average batch training loss.
         """
-        
-        return 0
-    
-    
+        pass
+
+    @abstractmethod
     def validate(self, val_loader: DataLoader) -> tuple[float, dict]:
         """
         Validate the model using the provided DataLoader.
+
         Args:
-            val_loader (DataLoader): DataLoader for the testing dataset.
-        
+            val_loader: DataLoader for the testing dataset.
+
         Returns:
-            tuple: A tuple containing the average test loss and a Metrics object.
+            A tuple containing the average test loss and a Metrics object.
         """
-        
-        return 0, {}
-        
-    def run(self, train_loader: DataLoader, val_loader: DataLoader, num_epochs: int, model_saving_metric: str = "Test Loss"):
+        pass
+
+    def run(
+        self,
+        train_loader: DataLoader,
+        val_loader: DataLoader,
+        num_epochs: int,
+        model_saving_metric: str = "Test Loss",
+    ):
         """
         Runs the training and testing loop for the specified number of epochs.
+
         Args:
-            train_loader (DataLoader): DataLoader for the training dataset.
-            val_loader (DataLoader): DataLoader for the testing dataset.
-            num_epochs (int): Number of epochs to run the training and testing loop.
+            train_loader: DataLoader for the training dataset.
+            val_loader: DataLoader for the testing dataset.
+            num_epochs: Number of epochs to run the training and testing loop.
+            model_saving_metric: The metric used to determine if the model
+                should be saved
         """
-        
-        
+
         for epoch in range(num_epochs):
             train_loss = self.train(train_loader)
             test_loss, metric = self.validate(val_loader)
-            
-            log_dict = {"Epoch": epoch, "Train Loss": train_loss, "Test Loss": test_loss,
-                           **metric}
+
+            log_dict = {
+                "Epoch": epoch,
+                "Train Loss": train_loss,
+                "Test Loss": test_loss,
+                **metric,
+            }
             self.logger(log_dict)
-                     
-   
+
             self._save_model(epoch, log_dict, model_saving_metric)
-    
-    
+
     def _save_model(self, epoch: int, log_dict: dict, model_saving_metric: str):
         """
         Saves the model state to a file.
+
         Args:
-            epoch (int): The current epoch number.
-            test_loss (float): The test loss at the current epoch.
+            epoch: The current epoch number.
+            log_dict: Dictionary containing the metrics for the current epoch.
+            model_saving_metric: The metric used to determine if the model
+                should be saved
         """
         if model_saving_metric not in log_dict:
-            raise Warning(f"Model saving metric '{model_saving_metric}' not found in log_dict")
+            raise Warning(
+                f"Model saving metric '{model_saving_metric}' not found in log_dict"
+            )
         metric_value = log_dict[model_saving_metric]
-        
+
         if metric_value <= self.min_metric_model:
             self.min_metric_model = metric_value
             if self.conf.save_model:
-                self.logger(f"Saving model at epoch {epoch} with {model_saving_metric} {metric_value}")
+                self.logger(
+                    f"Saving model at epoch {epoch} with "
+                    f"{model_saving_metric} {metric_value}"
+                )
                 torch.save(self.model, self.conf.get_checkpoint_path())
 
-
     @staticmethod
-    def inference(model: nn.Module, config: Config, logger: Logger = None):
+    @abstractmethod
+    def inference(model: nn.Module, config: Config, logger: Logger | None = None):
         """
-        Perform inference using the provided model. Static method since it does not require an instance of the class but it's useful to have it in the same class.
+        Perform inference using the provided model. Static method since it does
+        not require an instance of the class but it's useful to have it in the
+        same class.
+
         Args:
-            model (nn.Module): The model to be used for inference.
-            config (Config): Configuration object containing inference settings.
-            logger (Logger, optional): Logger instance for logging inference results. Defaults to None.
-        
+            model: The model to be used for inference.
+            config: Configuration object containing inference settings.
+            logger: Logger instance for logging inference results.
+
         Returns:
             Any: The result of the inference process.
         """
--- experiments/exp_toysort.py
+++ experiments/exp_toysort.py
@@ -1,32 +1,51 @@
+"""
+This module defines the ToySortExperiment class and its custom loss function.
+"""
+
 import torch
 from torch import nn, optim
 from torch.utils.data import DataLoader
-from utils.logger import Logger
-import torch.nn.functional as F
-from argparse import ArgumentParser
-from experiments.exp_base import BaseExperiment
+
 from configs.config import Config
+from experiments.exp_base import BaseExperiment
+from utils.logger import Logger
+
 
 class CustomCrossEntropyLoss(nn.Module):
     """
-    Custom cross entropy loss for transformer model with support for ignore_index.
+    Cross entropy loss for transformer model with support for ignore_index.
     """
-    def __init__(self, ):
+
+    def __init__(self) -> None:
         super().__init__()
-    
-    def forward(self, predictions, targets, ignore_index=-1):
-              
-        return F.cross_entropy(
-            predictions, 
-            targets, 
+
+    def forward(
+        self, predictions: torch.Tensor, targets: torch.Tensor, ignore_index: int = -1
+    ) -> torch.Tensor:
+        """
+        Computes the cross-entropy loss.
+
+        Args:
+            predictions: Predicted logits from the model
+            targets: Ground truth target indices
+            ignore_index: Specifies a target value that is ignored
+                and does not contribute to the input gradient.
+        """
+        return torch.nn.functional.cross_entropy(
+            predictions,
+            targets,
             ignore_index=ignore_index,
         )
+
 
 class ToySortExperiment(BaseExperiment):
     """
     Experiment class for the toy sorting task.
-    Handles sequence-to-sequence learning where the model learns to sort sequences.
+
+    Handles sequence-to-sequence learning where the model learns to sort
+    digit sequences in ascending order.
     """
+
     def __init__(
         self,
         model: nn.Module,
@@ -41,11 +60,18 @@ class ToySortExperiment(BaseExperiment):
         self.num_digits = conf.get("num_digits", 1)
 
     def train(self, train_loader: DataLoader) -> float:
+        """
+        Train the model for one epoch using the provided DataLoader.
 
+        Args:
+            train_loader: DataLoader for the training set.
+
+        Returns:
+            Average batch training loss.
+        """
         self.model.train()
         self.model.requires_grad_(True)
         total_loss = 0.0
-        
 
         for batch_idx, (x, y) in enumerate(train_loader):
             print(f"Train Batch {batch_idx + 1}/{len(train_loader)}", end="\r")
@@ -54,9 +80,11 @@ class ToySortExperiment(BaseExperiment):
             self.optimizer.zero_grad(set_to_none=True)
 
             logits = self.model(x)
-            
-            loss = self.criterion(logits.view(-1, logits.size(-1)), y.view(-1), ignore_index=-1)
-           
+
+            loss = self.criterion(
+                logits.view(-1, logits.size(-1)), y.view(-1), ignore_index=-1
+            )
+
             loss.backward()
             torch.nn.utils.clip_grad_norm_(self.model.parameters(), 1.0)
             self.optimizer.step()
@@ -66,11 +94,21 @@ class ToySortExperiment(BaseExperiment):
         return total_loss / len(train_loader)
 
     def validate(self, val_loader: DataLoader) -> tuple[float, dict]:
+        """
+        Validate the model using the provided DataLoader.
 
+        Args:
+            val_loader: DataLoader for the validation set.
+
+        Returns:
+            A tuple containing a placeholder value and a dictionary containing
+            validation metrics (accuracy, number of correctly predicted
+            sequences, and total number of validated sequences).
+        """
         self.model.eval()
         total_correct = 0
         total_sequences = 0
-        
+
         with torch.no_grad():
             for batch_idx, (x, y) in enumerate(val_loader):
                 print(f"Eval Batch {batch_idx + 1}/{len(val_loader)}", end="\r")
@@ -80,15 +118,15 @@ class ToySortExperiment(BaseExperiment):
                 inp = x[:, : self.sequence_length]
                 target_sol = y[:, -self.sequence_length :]
 
-                generated_full = self.model.generate(inp, max_length=self.sequence_length)
-                generated_sol = generated_full[:, self.sequence_length:]
-                
-                
+                generated_full = self.model.generate(
+                    inp, max_length=self.sequence_length
+                )
+                generated_sol = generated_full[:, self.sequence_length :]
+
                 correct = (generated_sol == target_sol).all(dim=-1)
                 total_correct += correct.sum().item()
                 total_sequences += generated_sol.size(0)
-                
-                        
+
         return 0, {
             "Accuracy": total_correct / total_sequences,
             "Correct Sequences": total_correct,
@@ -96,16 +134,23 @@ class ToySortExperiment(BaseExperiment):
         }
 
     @staticmethod
-    def inference(model: nn.Module, config: Config, logger: Logger = None):
+    def inference(model: nn.Module, config: Config, logger: Logger | None = None):
+        """
+        Run inference on a random input sequence and log the results.
+
+        Args:
+            model: The model to use for inference.
+            config: Configuration object.
+            logger: Logger instance for logging results.
+                If None, prints to console.
+        """
         input = torch.randint(0, model.num_digits, (1, 6), dtype=torch.long)
         model.eval()
         with torch.no_grad():
-            
             generated = model.generate(input, max_length=6)
-            
-        if not logger:
-            logger = print # very pythonic :)
-        
+
+        if logger is None:
+            logger = print  # very pythonic :)
+
         logger("Input Sequence:", input.tolist()[0])
-        logger("Generated Sequence:", generated.tolist()[0][6:])    
-            
+        logger("Generated Sequence:", generated.tolist()[0][6:])
